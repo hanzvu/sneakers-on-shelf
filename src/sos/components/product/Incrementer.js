a9bf2d75864f32fa64af73a0a6e77f2227f24baa
Nguyễn Văn Hoàng <nguyenhoanghh2002@gmail.com>
2022-11-30 11:00:26 +0700

    custom product detail

--- src/sos/components/product/Incrementer.js
+++ src/sos/components/product/Incrementer.js
@@ -1,4 +1,4 @@
-import { Box, IconButton, TextField } from "@mui/material";
+import { Box, IconButton, Stack, TextField } from "@mui/material";
 import Iconify from "../../../components/Iconify";
 
 export default function Incrementer({ available, quantity, onIncrementQuantity, onDecrementQuantity, onChangeQuantity }) {
@@ -9,21 +9,30 @@ export default function Incrementer({ available, quantity, onIncrementQuantity, 
                 px: 0.75,
                 border: 1,
                 lineHeight: 0,
-                borderRadius: 1,
+                height: 49,
                 display: 'flex',
-                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 1,
                 borderColor: 'grey.50032',
             }}
         >
-            <IconButton size="small" color="inherit" disabled={quantity <= 1} onClick={onDecrementQuantity}>
-                <Iconify icon={'eva:minus-fill'} width={14} height={14} />
-            </IconButton>
+            <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                spacing={2}
+            >
+                <IconButton size="small" color="inherit" disabled={quantity <= 1} onClick={onDecrementQuantity}>
+                    <Iconify icon={'eva:minus-fill'} width={14} height={14} />
+                </IconButton>
 
-            <TextField variant="standard" value={quantity} onChange={onChangeQuantity} inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', min: 1, style: { textAlign: 'center' } }} sx={{ width: 40, textAlign: 'center' }} />
+                <TextField variant="standard" value={quantity} onChange={onChangeQuantity} inputProps={{ inputMode: 'numeric', pattern: '[0-9]*', min: 1, style: { textAlign: 'center' } }} sx={{ width: 40, textAlign: 'center' }} />
 
-            <IconButton size="small" color="inherit" disabled={quantity >= available} onClick={onIncrementQuantity}>
-                <Iconify icon={'eva:plus-fill'} width={14} height={14} />
-            </IconButton>
+                <IconButton size="small" color="inherit" disabled={quantity >= available} onClick={onIncrementQuantity}>
+                    <Iconify icon={'eva:plus-fill'} width={14} height={14} />
+                </IconButton>
+            </Stack>
+
         </Box>
     );
 }
